13ba801b5caf92df06e7b7eb47a10f59c0b0139c
Иван Самусёв <samusyov.spam@gmail.com>
2022-03-05 11:40:59 +0300

    lesson 01 has been done recently...

--- src/by/it/group973603/Samusyov/lesson01/FiboC.java
+++ src/by/it/group973603/Samusyov/lesson01/FiboC.java
@@ -19,7 +19,7 @@ public class FiboC {
     public static void main(String[] args) {
         FiboC fibo = new FiboC();
         int n = 10;
-        int m = 4;
+        int m = 2;
         System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n, m), fibo.time());
     }
 
