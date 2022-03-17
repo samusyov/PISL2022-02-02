aed39d369e214e2715a055879a69a265e32dbe65
july <yliaglad123@gmail.com>
2022-03-18 01:21:24 +0300

    Lesson 6-8(A,B,C)

---
+++ src/by/it/group973601/gladkouskaya/lesson07/A_EditDist.java
@@ -0,0 +1,82 @@
+package by.it.group973601.gladkouskaya.lesson07;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Рекурсивно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 3:
+    5
+
+*/
+
+public class A_EditDist {
+
+
+    int getDistanceEdinting(String one, String two) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+        int result = 0;
+        if (one.isEmpty())
+            return two.length();
+
+        if (two.isEmpty())
+            return one.length();
+
+        int[] options = new int[3];
+
+        options[0] = getDistanceEdinting(
+                one.substring(1), two.substring(1))
+                + (one.charAt(0) == two.charAt(0) ? 0 : 1);
+
+        options[1] = getDistanceEdinting(
+                one.substring(1), two) + 1;
+
+        options[2] = getDistanceEdinting(
+                one, two.substring(1)) + 1;
+
+        result = Arrays.stream(options).min().orElse(Integer.MAX_VALUE);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        return result;
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/group973601/gladkouskaya/lesson07/dataABC.txt");
+        A_EditDist instance = new A_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+    }
+}
